9f3cc4dfc97672603c3d038dd318c311a2c7cdf8
Geovanny Alvarez <geovannyalvarez504@gmail.com>
2023-07-18 16:16:55 -0600

    first commit

---
+++ console-CSharp/MyClass.cs
@@ -0,0 +1,31 @@
+﻿namespace MyFirstClass;
+
+public class MyClass
+{
+    // BODY
+    
+    // Constantes
+    const double pi = 3.14;
+    const double iva = 0.15;
+
+    // Propiedades
+    // son variables que pertenecen a una clase
+    public int Number;
+    public int Number2 = 3; // Se pueden iniciar
+    public string Name { get; set; } // Dar nivel de accesibilidad get: Podemos obtener; set: Podemos setear; depende el nivel de acceso
+    public int old { get; private set; }
+    public readonly string dni; // Solo lectura a diferencia de const no necesita declarar su valor al momento de compilacion.
+
+    // Metodos
+    public string Metodo(int old)
+    {
+        return "you're " + old + " years old";
+    }
+    // Eventos
+    // Indizadores
+    // Operadores
+    // Constructores
+    // Desctructores
+    // Tipos
+
+}
---
+++ console-CSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using MyFirstClass;
+
+namespace Program;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass myClass = new MyClass();
+
+        Console.WriteLine(myClass.Metodo(5));
+    }
+}
